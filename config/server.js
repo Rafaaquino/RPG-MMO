daf411b241a75f1e8fb1bfdd218bbffb4d012372
Rafael Aquino <rafaaquino28@gmail.com>
2020-12-01 18:38:32 -0300

    configurando express session no server da aplicação

--- config/server.js
+++ config/server.js
@@ -10,6 +10,15 @@ var bodyParser = require('body-parser');
 /* importar o módulo do express-validator */
 var expressValidator = require('express-validator');
 
+
+/* importar o módulo do express-session */
+var expressSession = require('express-session');
+
+
+
+
+
+
 /* iniciar o objeto do express */
 var app = express();
 
@@ -26,6 +35,16 @@ app.use(bodyParser.urlencoded({extended: true}));
 /* configurar o middleware express-validator */
 app.use(expressValidator());
 
+
+/* configurar o middleware express-session */
+app.use(expressSession({
+	
+	secret: '',
+	resave: false,
+	saveUninitialized: false	
+}));
+
+
 /* efetua o autoload das rotas, dos models e dos controllers para o objeto app */
 consign()
 	.include('app/routes')
@@ -33,6 +52,8 @@ consign()
 	.then('app/models')
 	.then('app/controllers')
 	.into(app);
+
+
 
 /* exportar o objeto app */
 module.exports = app;
