5f8993e71c0c543615faed11d2edaeaaa8cfd784
Rafael Aquino <rafaaquino28@gmail.com>
2020-12-01 18:27:53 -0300

    ajustes na função express session

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,3 +1,23 @@
 module.exports.index = function(application, req, res){
-	res.render('index');
+	res.render('index', {validacao: {}});
+}
+
+
+module.exports.autenticar = function(application, req, res){
+	
+	var dadosForm = req.body;
+
+	req.assert('usuario', 'Usúario é obrigatorio').notEmpty();
+	req.assert('senha', 'Senha é obrigatorio').notEmpty();
+
+	var erros = req.validationErrors();
+
+
+	if(erros){
+
+		res.render('index', {validacao: erros});
+		return;
+	}
+
+	res.send('tudo ok para autenticar');
 }
