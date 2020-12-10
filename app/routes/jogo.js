910946f3be3a5e64a9ff73e6ac5ac14e018b64c8
Rafael Aquino <rafaaquino28@gmail.com>
2020-12-10 18:30:29 -0300

    exibindo habilidades na view

--- app/routes/jogo.js
+++ app/routes/jogo.js
@@ -3,6 +3,14 @@ module.exports = function(application){
 		application.app.controllers.jogo.jogo(application, req, res);
 	});
 
+	application.get('/sudito', function(req, res){
+		application.app.controllers.jogo.sudito(application, req, res);
+	});
+
+	application.get('/pergaminho', function(req, res){
+		application.app.controllers.jogo.pergaminho(application, req, res);
+	});
+
 	application.get('/sair', function(req, res){
 		application.app.controllers.jogo.sair(application, req, res);
 	});
